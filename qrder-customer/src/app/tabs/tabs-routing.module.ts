5d50e08c4c7f70e9c23ca691896986d5e92c6200
Thomas Maier <thomas.maier@tractive.com>
2021-04-26 23:34:34 +0200

    Reduce tab amount and adjust the two remaining tabs

--- qrder-customer/src/app/tabs/tabs-routing.module.ts
+++ qrder-customer/src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,23 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'menutab',
+        loadChildren: () => import('../menutab/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'carttab',
+        loadChildren: () => import('../carttab/tab2.module').then(m => m.Tab2PageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/menutab',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/menutab',
     pathMatch: 'full'
   }
 ];
