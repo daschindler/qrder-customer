031fc97a962454ee67e726369dce00855bbe9586
Julian Janisch <julian.janisch96@gmail.com>
2021-05-06 16:52:47 +0200

    Add clear button and clear alert controller

--- qrder-customer/src/app/carttab/tab2.page.ts
+++ qrder-customer/src/app/carttab/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,9 +9,40 @@ import { Component } from '@angular/core';
 export class Tab2Page {
   qrData = 'zack brack qr code ins gnack';
   qrColor;
-  constructor() {
+  constructor(public alertController: AlertController) {
     // Use matchMedia to check the user preference
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
     this.qrColor = prefersDark.matches ? '#ffffff' : '#000000';
+  }
+  clickedClear() {
+    // Clear the cart
+  }
+
+  async presentClearAlert() {
+    const alert = await this.alertController.create({
+      header: 'Are you sure?',
+      message: 'Do you really want to delete your order? The shopping cart will be empty afterwards.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Clicked cancel');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            console.log('Clicked Yes on clear alert.');
+            this.clickedClear();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 }
